043ce028512792427c0ed70865ed92bd61a5743e
jonathanbutaye <jonathan.butaye@student.uhasselt.be>
2021-12-24 12:26:52 +0100

    try first assertion

--- ahb_arbiter_wrapper.sv
+++ ahb_arbiter_wrapper.sv
@@ -18,7 +18,13 @@ module ahb_arbiter_wrapper (
         HREADY, HMASTER, HMASTLOCK
     );
 
-    /* hic sunt dracones */    
+    /* hic sunt dracones */
+    /* grant can never been given to more than one master */
+    /* simple trick to check wether a number has more than one bit set: n & (n - 1) != 0 */
+    initial 
+    begin
+        no_multiple_grants: assert (HGRANTx & (HGRANTx - 1) != 0) $display("%m pass"); else $info("%m fail");
+    end
     
 
 endmodule : ahb_arbiter_wrapper
